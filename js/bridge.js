24736500707ecc3cbd68e9bdf70cf0e11636e631
Gabriel Harel <pabombs@gmail.com>
2016-07-24 00:39:06 -0400

    ....

--- js/bridge.js
+++ js/bridge.js
@@ -212,7 +212,6 @@
                         source_index = 6;
                         target_index = 7;
                     }
-                    user.tasks[index][target_index] = 1;
                     draw.tasks()();
                 });
             },
