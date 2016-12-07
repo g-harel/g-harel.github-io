aaeae47a053a8dc1058c5bb0fbb19ee586eeee7f
Gabriel <pabombs@gmail.com>
2016-12-06 22:18:57 -0500

    scroll buttons now working

--- Sayra/script.js
+++ Sayra/script.js
@@ -6,11 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('menu').style.opacity = 1;
     }, 0);
 
-    // scrolls to the begining of content
-    document.getElementById('more').addEventListener('click', function() {
-        scrollTo(window.innerHeight, 1000);
-    });
-
     // send for data back to server
     document.getElementById('logo_main').addEventListener('click', function() {
         var xhr = new XMLHttpRequest();
@@ -39,36 +34,41 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-});
+    // scrolls to target in duration with easing
+    window.scrollTo = function(target, duration) {
+        // finding place to scroll to
+        var element = document.getElementById(target);
+        if (!element) {
+            return 0;
+        }
+        target = (element.getBoundingClientRect().top + window.pageYOffset || document.documentElement.scrollTop) - 45;
+        // defaults
+        duration = duration || 1000;
+        // does not use animations if browser is too old
+        if (!window.requestAnimationFrame && !(window.scrollY || document.documentElement.scrollTop)) {
+            console.log('please update your browser for best experience');
+            if (window.scoll) {
+                window.scroll(Xscroll, target);
+            }
+            return;
+        }
+        // variables
+        var Xscroll = window.scrollX || document.documentElement.scrollLeft || 0,
+            Yscroll = window.scrollY || document.documentElement.scrollTop || 0,
+            startTime;
+        // animates the scroll to the target value
+        window.requestAnimationFrame(scrolldown);
+        function scrolldown(time) {
+            startTime = startTime || time;
+            time -= startTime;
+            var completion = (0.5 * Math.cos( Math.PI * (time/duration) + Math.PI ) + 0.5);
+            window.scroll(Xscroll, Yscroll+(target-Yscroll)*completion);
+            if (time < duration) {
+                window.requestAnimationFrame(scrolldown);
+            } else {
+                window.scroll(Xscroll, target);
+            }
+        }
+    }
 
-// scrolls to target in duration with easing
-function scrollTo(target, duration) {
-    // defaults
-    duration = duration || 1000;
-    target = target || 0;
-    // does not use animations if browser is too old
-    if (!window.requestAnimationFrame && !(window.scrollY || document.documentElement.scrollTop)) {
-        console.log('please update your browser for best experience');
-        if (window.scoll) {
-            window.scroll(Xscroll, target);
-        }
-        return;
-    }
-    // variables
-    var Xscroll = window.scrollX || document.documentElement.scrollLeft || 0,
-        Yscroll = window.scrollY || document.documentElement.scrollTop || 0,
-        startTime;
-    // animates the scroll to the target value
-    window.requestAnimationFrame(scrolldown);
-    function scrolldown(time) {
-        startTime = startTime || time;
-        time -= startTime;
-        var completion = (0.5 * Math.cos( Math.PI * (time/duration) + Math.PI ) + 0.5);
-        window.scroll(Xscroll, Yscroll+(target-Yscroll)*completion);
-        if (time < duration) {
-            window.requestAnimationFrame(scrolldown);
-        } else {
-            window.scroll(Xscroll, target);
-        }
-    }
-}
+});
