d140a93cf5ed6affb0482a371d243143c62315ff
Gabriel <gabrielj.harel@gmail.com>
2018-11-17 02:38:28 -0500

    refactor into flat structure + add contribution fetching/rendering

--- config.go
+++ config.go
@@ -1,31 +1,55 @@
 package main
 
+import (
+	"os"
+)
+
 // Config configures how the output is generated.
 type Config struct {
-	Username string
-	Projects []string
+	Templates string
+	RootName  string
+	OutPath   string
+
+	Endpoint string
+	Token    string
+
+	Request *Request
 }
 
-var config = Config{
-	Username: "g-harel",
-	Projects: []string{
-		"okwolo/okwolo",
-		"g-harel/rickety",
-		"g-harel/svgsaurus",
-		"g-harel/ence",
-		"g-harel/slurry",
-		"g-harel/coco",
-		"g-harel/g-harel.github.io",
-		"g-harel/superpermutations",
-		"g-harel/paph",
-		"g-harel/cover-gen",
-		"g-harel/http",
-		"g-harel/targetblank",
-		"g-harel/663",
-		"g-harel/game-of-life",
-		"g-harel/num-match",
-		"g-harel/open-source-logos",
-		"g-harel/diip",
-		"g-harel/emn",
+var config = &Config{
+	Templates: "templates",
+	RootName:  "index.tmpl",
+	OutPath:   "index.html",
+
+	Endpoint: "https://api.github.com/graphql",
+	Token:    os.Getenv("GITHUB_API_TOKEN"),
+
+	Request: &Request{
+		Login: "g-harel",
+		Projects: []ProjectRequest{
+			{"okwolo", "okwolo"},
+			{"g-harel", "rickety"},
+			{"g-harel", "svgsaurus"},
+			{"g-harel", "ence"},
+			{"g-harel", "slurry"},
+			{"g-harel", "coco"},
+			{"g-harel", "g-harel.github.io"},
+			{"g-harel", "superpermutations"},
+			{"g-harel", "paph"},
+			{"g-harel", "cover-gen"},
+			{"g-harel", "http"},
+			{"g-harel", "targetblank"},
+			{"g-harel", "663"},
+			{"g-harel", "game-of-life"},
+			{"g-harel", "num-match"},
+			{"g-harel", "open-source-logos"},
+			{"g-harel", "diip"},
+			{"g-harel", "emn"},
+		},
+		Contributions: []ContributionRequest{
+			{"facebook", "jest", 7170, 7165},
+			{"golang", "dep", 1755, 1754},
+			{"DefinitelyTyped", "DefinitelyTyped", 26589, 0},
+		},
 	},
 }
--- main.go
+++ main.go
@@ -1,48 +1,56 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"time"
-
-	"github.com/g-harel/g-harel.github.io/query"
-	"github.com/g-harel/g-harel.github.io/render"
 )
+
+func fatal(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, "\033[31;1mfatal error: %s\033[0m\n", fmt.Sprintf(format, a...))
+	os.Exit(1)
+}
 
 func main() {
 	start := time.Now()
 
-	token, ok := os.LookupEnv("GITHUB_API_TOKEN")
-	if !ok {
-		return
-	}
-	client := query.Client{
-		Queries: "query",
-		URL:     "https://api.github.com/graphql",
-		Token:   token,
-	}
-
-	user, err := client.User(config.Username)
+	requestData, err := config.Request.Marshall()
 	if err != nil {
 		panic(err)
 	}
 
-	projects, err := client.Projects(config.Projects)
+	req, err := http.NewRequest("POST", config.Endpoint, bytes.NewReader(requestData))
 	if err != nil {
-		panic(err)
+		fatal("could not create http request: %v", err)
 	}
 
-	agent := render.Agent{
-		Templates: "render",
-		RootName:  "index.tmpl",
-		OutPath:   "index.html",
-
-		User:     user,
-		Projects: projects,
+	if config.Token != "" {
+		req.Header.Add("Authorization", fmt.Sprintf("bearer %v", config.Token))
 	}
-	err = agent.Execute()
+
+	res, err := (&http.Client{}).Do(req)
 	if err != nil {
-		panic(err)
+		fatal("query request failed: %v", err)
+	}
+
+	responseData := &bytes.Buffer{}
+	_, err = io.Copy(responseData, res.Body)
+	if err != nil {
+		fatal("could not read response data: %v", err)
+	}
+
+	response := &Response{}
+	err = response.Unmarshall(responseData.Bytes())
+	if err != nil {
+		fatal("could not parse response data: %v", err)
+	}
+
+	err = Render(config, response)
+	if err != nil {
+		fatal("could not render templates: %v", err)
 	}
 
 	fmt.Printf("Built from fresh data in %s\n", time.Since(start))
